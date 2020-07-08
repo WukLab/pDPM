590ac29d6b5989aac59c06b98992ad04367277b6
Yizhou Shan <syzwhat@gmail.com>
2020-07-08 11:29:41 -0700

    Clover:Rename run_server.sh to run_ms.sh

---
+++ clover/run_ms.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+Help=$(cat <<-"HELP"
+
+run_ms.sh - Run a Clover Metadata Server
+
+Usage:
+    ./run_ms.sh <id>
+
+Options:
+    <id>   Unique Clover Node ID.
+    -h     Show this message.
+
+Examples:
+    ./run_ms.sh 0
+
+HELP
+)
+
+help() {
+	echo "$Help"
+}
+
+if [[ $# == 0 ]] || [[ "$1" == "-h" ]]; then
+	help
+	exit 1
+fi
+
+#LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes numactl --physcpubind=0 --localalloc ./init -b 1 -S 1 -I $1 -d $device -L 2
+./init -b 1 -S 1 -I $1 -d $device -L 2
